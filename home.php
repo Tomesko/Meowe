85e4a47ffbf10ee1f741096d202aa353e86b07f3
Tomesko <117730575+Tomesko@users.noreply.github.com>
2024-03-12 13:36:51 +0100

    Add files via upload

---
+++ home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<link rel="stylesheet" href="Hotbar.css">
+
+</head>
+<body>
+
+<?php
+include("Hotbar.html");
+?>
+
+<div style="padding:20px;margin-top:30px;background-color:white;height:1500px;">
+  <h1>Spawn point</h1>
+
+  <h2>Ther's gonna be links to informations about game :)</h2>
+  
+</div>
+
+</body>
+</html>
---
+++ novinky.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<link rel="stylesheet" href="Hotbar.css">
+
+</head>
+<body>
+
+  <?php
+include("Hotbar.html");
+?>
+
+
+
+<div style="padding:20px;margin-top:30px;background-color:white;height:1500px;">
+  <h1>News</h1>
+
+  <h2>Updates</h2>
+
+  <h3>crossovcer</h3>
+ 
+</div>
+
+</body>
+</html>
